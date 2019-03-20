e600757c67481396b33a7a38c8e2fbd96436737e
SambaGod <48761205+SambaGod@users.noreply.github.com>
2019-03-20 14:55:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title>My Samba Diary</title>
+  <link rel="stylesheet" href="CSS/my-style.css" />
+</head>
+<body>
+
+
+<header></header>
+
+<marquee style="border: 2px solid red; color: red; margin-top: 20px; margin-bottom: 5px;">
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+Page Under Construction &nbsp; &nbsp; &nbsp;
+</marquee>
+
+<div id="my-wrapper">
+
+	<div id="my-login-image"></div>
+	<div id="my-login-form"></div>
+
+</div>
+
+<footer></footer>
+
+</body>
+</html>
