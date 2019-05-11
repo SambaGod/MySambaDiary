3ec689979e3991e27f7fc1f54d4442d1f8d0af0e
SSSvensk <sss2341@hotmail.com>
2019-05-12 01:05:21 +0300

    admin page sidebar

--- sambaproject/src/views/Admin.vue
+++ sambaproject/src/views/Admin.vue
@@ -1,13 +1,44 @@
-<template>  
-    <div>    
-        <h2>Admin</h2>    
-        <p>Welcome, {{ user }}</p>
-        <b-button v-on:click="logout">Logout</b-button>
-    </div>
+<template>
+  <div>
+    <Sidebar></Sidebar>
+    <v-content>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex md4 xs12>
+            <v-card color="primary">
+              <v-card-text class="px-0">12</v-card-text>
+                </v-card>
+              </v-flex>
+          <v-flex md4 xs12>
+            <v-card dark color="primary">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex md4 xs12>
+            <v-card dark color="primary">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <div>    
+          <h2>Admin</h2>    
+          <p>Welcome, {{ user }}</p>
+          <b-button v-on:click="logout">Logout</b-button>
+          <div>Add school leader</div>
+          <div>Remove users</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </v-container>
+    </v-content>
+  </div>
 </template>  
 <script>  
-    import axios from "axios"
-    import router from "../router"    
+import axios from "axios"
+import router from "../router"
+import Sidebar from "@/components/Sidebar"
+
     export default {    
         name: "Login",    
         data() {    
@@ -16,7 +47,10 @@
                     name: "Defadmin"   
                 }    
             }    
-        },    
+        },
+        components: {
+            Sidebar
+        },
         methods: {
             logout() {
                 axios.get("/api/logout")
