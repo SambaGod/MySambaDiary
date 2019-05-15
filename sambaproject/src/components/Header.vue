63ce066f860c4e5b25635eb1f5b1f84fd0d34505
SambaGod <48761205+SambaGod@users.noreply.github.com>
2019-05-15 16:03:00 +0200

    Add files via upload

---
+++ sambaproject/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link>
+</div>
+</template>
+
+<script>
+export default {
+    name: "navigation"
+}
+</script>
+
+
+<style scoped>
+
+#nav {
+    width: 1000px;
+    margin: 0 auto;
+    border: solid 2px black;
+}
+
+
+</style>
+
