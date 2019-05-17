f795ac93ab438eef7d662e8c5397bbb95ad400e5
SambaGod <48761205+SambaGod@users.noreply.github.com>
2019-05-17 11:28:15 +0200

    Add files via upload

--- sambaproject/src/views/Home.vue
+++ sambaproject/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
 <div id="homeBod">
   <Header />
-  <div>Home</div>
+  <div id="wrap">
+    <div id="sec1">&nbsp;</div>
+    <div id="sec2">
+      <div class="sec2l">Left</div>
+      <div class="sec2r">Right</div>
+    </div>
+    <div id="sec3">&nbsp;</div>
+  </div>
  
 <footer></footer>
 </div>
@@ -26,20 +33,51 @@ export default {
 
 #homeBod{
   background:url("../assets/hBack.jpeg") no-repeat center;
-  background-size: 110%;
+  background-size: 150%;
 }
-#lwrap {
+#wrap {
 
-  width:1000px;
+  width:900px;
   border: 2px solid black;
   margin: 0 auto;
   margin-top: 100px;
 
 }
 
+#sec1{
+  height:200px;
+  border:2px solid aqua;
+  background: #fff;
+}
+
+#sec2{
+  display: grid;
+  grid-template-columns: 500px 400px;
+  height: 500px;
+  color: red;
+  font-size: 72pt;
+}
+
+.sec2l{
+  border:2px solid green;
+  background:#fff;
+}
+
+.sec2r{
+  border:2px solid greenyellow;
+  background:rgba(0,0,0,0.3);
+}
+
+#sec3{
+  height:200px;
+  border:2px solid purple;
+  display: block;
+  background: #fff;
+}
+
 footer {
-  margin-top: 300px;
-  height: 50px;
+  margin-top: 100px;
+  height: 200px;
   border: 2px solid black;
   background: #111;
 }
