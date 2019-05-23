261d093c1e6ffdf11a29f0dff27328af3c52928f
SambaGod <48761205+SambaGod@users.noreply.github.com>
2019-05-23 16:28:08 +0200

    Changed footer to a component

---
+++ sambaproject/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-footer dark height="auto" class="pt-2">
+  <v-card
+      class="flex indigo"
+      flat
+      tile
+    >
+      <v-card-title class="red">
+        <strong class="subheading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatem quas natus voluptates ducimus ipsam doloribus dicta impedit laborum quidem praesentium et eveniet repudiandae dolorum soluta rerum, porro enim molestias.</strong>
+        
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;&nbsp;2019 — <strong>Samba Diary</strong>
+      </v-card-actions>
+    </v-card>
+</v-footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
